40fd593f2d71ad08ef65e23e75fdb4ff951e5271
Arvind Prakash <arvindprakasht23@gmail.com>
2026-03-20 11:26:05 +0530

    Automated code update from Windows PC

--- parser.c
+++ parser.c
@@ -12,13 +12,18 @@ void parse_and_store(char *input) {
     for (int i = 0; i < DICT_SIZE; i++) {
         if (strcmp(cmd_name, cmd_dict[i].cmd_string) == 0) {
             uint32_t hw_offset = 0;
+            int arg_idx = 0;
             
-            for (int arg_idx = 0; arg_idx < cmd_dict[i].min_args; arg_idx++) {
+            // Keep looping as long as there are comma-separated values
+            while (1) {
                 char *token = strtok(NULL, ", )");
-                if (!token) break;
+                if (!token) break; // Exit loop when no more arguments are found
 
                 uint32_t val = (uint32_t)strtoul(token, NULL, 16);
-                arg_type_t current_type = cmd_dict[i].arg_format[arg_idx];
+                
+                // If we have more tokens than min_args, lock the type to the last argument type (which is ARG_ARRAY_U8 for bursts)
+                int format_idx = (arg_idx < cmd_dict[i].min_args) ? arg_idx : (cmd_dict[i].min_args - 1);
+                arg_type_t current_type = cmd_dict[i].arg_format[format_idx];
 
                 if (current_type == ARG_U8 || current_type == ARG_ARRAY_U8) {
                     uint8_t temp8 = (uint8_t)(val & 0xFF);
@@ -30,7 +35,10 @@ void parse_and_store(char *input) {
                     memcpy((void*)&HW_OPERAND_BASE[hw_offset], &temp16, 2);
                     hw_offset += 2;
                 }
+                
+                arg_idx++;
             }
+            
             WRITE_OPCODE(cmd_dict[i].opcode);
             WRITE_CMD(1); 
             return;
